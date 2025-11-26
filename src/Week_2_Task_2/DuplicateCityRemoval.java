28db9099269bb35c10bee02226cb7ab4ef413a39
yunusBagewadi529 <yunus.bagewadi32@gmail.com>
2025-11-26 06:31:05 +0530

    Set Collection Data Structures Commit

---
+++ src/Week_2_Task_2/DuplicateCityRemoval.java
@@ -0,0 +1,22 @@
+package Week_2_Task_2;
+
+import java.util.Scanner;
+import java.util.LinkedHashSet;
+
+public class DuplicateCityRemoval {
+    public static void main(String[] args) {
+        LinkedHashSet<String> cities = new LinkedHashSet<>();
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter Your City Name: ");
+            String city = sc.nextLine();
+
+            cities.add(city);
+        }
+
+        System.out.println("\nList Created With Unique Cities: \n" + cities);
+
+        sc.close();
+    }
+}
